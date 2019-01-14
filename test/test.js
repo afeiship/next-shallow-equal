43ea886e17eff161c7a0a7755f438d3ab84870a1
afeiship <afeiship@gmail.com>
2019-01-14 23:03:36 +0800

    feat: add test case

--- test/test.js
+++ test/test.js
@@ -1,15 +1,26 @@
 var nx = require('next-js-core2');
 require('../src/next-shallow-equal');
 
+test('nx.shallowEqual: shallow object', function() {
+  var obj1 = { name: 'fei' };
+  var obj2 = { name: 'fei' };
+  var rs = nx.shallowEqual(obj1, obj2);
 
-test('nx.shallowEqual', function () {
-  var obj1 = {name: 'fei'};
-  var obj2 = {email: '1290657123@qq.com'};
-
-  var result = {};
-
-  nx.shallowEqual(result, obj1, obj2);
-
-  expect(result.name, obj1.name).toBe(null);
+  expect(rs).toBe(true);
 });
 
+test('nx.shallowEqual: the same object', function () {
+  var obj1 = { name: 'fei' };
+  var obj2 = obj1;
+  var rs = nx.shallowEqual(obj1, obj2);
+
+  expect(rs).toBe(true);
+});
+
+test('nx.shallowEqual: not the same object', function () {
+  var obj1 = { name: 'fei' };
+  var obj2 = { name: 'fei', age: 108};
+  var rs = nx.shallowEqual(obj1, obj2);
+
+  expect(rs).toBe(false);
+});
